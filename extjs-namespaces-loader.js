f4fe3332398b87416a124178a8d44ef7cea09039
iMakss <m.aldanov@niitp.in>
2021-02-05 01:01:24 +0300

    Разделил на 2 загрузчика

---
+++ extjs-namespaces-loader.js
@@ -0,0 +1,8 @@
+/*
+	MIT License http://www.opensource.org/licenses/mit-license.php
+	Author Aldanov Maksim
+*/
+const loader = require('./lib/namespaces')
+
+module.exports = loader.default
+// module.exports
